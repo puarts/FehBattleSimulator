4cee633081f29b01f6729ec0a6bf407104b47813
puarts <dummy@dummy.com>
2020-09-26 22:55:58 +0900

    APIリファレンスからクラス一覧が消えてしまっていたのを修正

--- Documents/Api/html/menudata.js
+++ Documents/Api/html/menudata.js
@@ -23,4 +23,8 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"総合概要",url:"index.html"}]}
+{text:"総合概要",url:"index.html"},
+{text:"クラス",url:"annotated.html",children:[
+{text:"クラス一覧",url:"annotated.html"},
+{text:"クラス索引",url:"classes.html"},
+{text:"クラス階層",url:"hierarchy.html"}]}]}
--- Documents/Api/html/search/searchdata.js
+++ Documents/Api/html/search/searchdata.js
@@ -1,12 +1,18 @@
 var indexSectionsWithContent =
 {
+  0: "abcdefhikmoqstuw",
+  1: "abcdefhikmoqstuw"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes"
 };
 
 var indexSectionLabels =
 {
+  0: "全て",
+  1: "クラス"
 };
 
