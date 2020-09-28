44039c77046853497af47e5867815e5ddd844d30
puarts <dummy@dummy.com>
2020-09-28 22:20:41 +0900

    テンプレートマッチング用の画像を追加

--- Sources/GlobalDefinitions.js
+++ Sources/GlobalDefinitions.js
@@ -1,6 +1,7 @@
 
 const g_siteRootPath = "https://puarts.com/";
 const g_imageRootPath = g_siteRootPath + "AetherRaidTacticsBoard/images/";
+const g_corsImageRootPath = "../images/"; // クロスオリジン制約を回避するため画像認識のテンプレート等はローカル画像を参照
 const g_audioRootPath = g_siteRootPath + "AetherRaidTacticsBoard/audio/";
 const TurnSettingCookiePrefix = "turn_";
 const UnitCookiePrefix = "unit_";
