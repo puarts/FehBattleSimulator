7a55bd22625a66e0e3c64307b687d5ce24d19121
puarts <71858166+puarts@users.noreply.github.com>
2020-10-02 11:27:45 +0900

    Deploy.batの修正

--- Deploy.bat
+++ Deploy.bat
@@ -1,8 +1,8 @@
 @echo off
 SetLocal EnableDelayedExpansion
-set JSMIN=F:\trunk\PortableApplications\JSMin-master\jsmin.bat
-set destination=F:\trunk\Websites\puarts.com\AetherRaidTacticsBoard\Release2
-set deply_filenames=Main.js,Unit.js,Skill.js,AetherRaidDefensePresets.js,AppData.js,AudioManager.js,DamageCalculator.js,Map.js,Setting.js,StatusCalcMain.js,Structures.js,Table.js,TurnSetting.js,Utilities.js
+set JSMIN=%~dp0..\..\..\PortableApplications\JSMin-master\jsmin.bat
+set destination=%~dp0..\AetherRaidTacticsBoard\Release2
+set deply_filenames=Main.js,Unit.js,Skill.js,AetherRaidDefensePresets.js,AppData.js,AudioManager.js,DamageCalculator.js,Tile.js,Map.js,SettingManager.js,StatusCalcMain.js,Structures.js,Table.js,TurnSetting.js,Utilities.js
 
 for %%n in (%deply_filenames%) do (
     xcopy /y %~dp0Sources\%%n %destination%
