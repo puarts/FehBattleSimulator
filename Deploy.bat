f33e268de496a9a2a7fc3eec2b87bdf0007da2d9
puarts <71858166+puarts@users.noreply.github.com>
2020-12-28 19:17:15 +0900

    Deploy.batのファイル名入力ミス修正

--- Deploy.bat
+++ Deploy.bat
@@ -2,7 +2,7 @@
 set JSMIN=%~dp0..\..\..\PortableApplications\JSMin-master\jsmin.bat
 set destination=%~dp0..\AetherRaidTacticsBoard\Release2
 set output_js=%destination%\FehBattleSimulator.js
-set filenames=GlobalDefinitions,VueComponent,Utilities,Skill,Tile,Map,Structures,Table,Unit,DamageCalculator,TurnSetting,AudioManager,AetherRaidDefensePresets,AppData,SettingManager,Main_ImageProcessing,Main_OriginalAi,Main_MouseAndTouch,Main
+set filenames=GlobalDefinitions,VueComponents,Utilities,Skill,Tile,Map,Structures,Table,Unit,DamageCalculator,TurnSetting,AudioManager,AetherRaidDefensePresets,AppData,SettingManager,Main_ImageProcessing,Main_OriginalAi,Main_MouseAndTouch,Main
 if exist %output_js% del %output_js%
 break>%output_js%
 for %%n in (%filenames%) do (
