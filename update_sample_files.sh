ca5a47b0db2b68939f9fcf1b1343c944543cc8cc
yakitori-776 <mac-mini@makkunoMac-mini.local>
2022-01-27 21:55:06 +0900

    サンプル更新スクリプトの修正

--- update_sample_files.sh
+++ update_sample_files.sh
@@ -2,7 +2,7 @@
 
 curl -k "https://puarts.com/?pid=1469" > source.html
 
-grep "^var heroInfos" source.html > Sources/SampleHeroInfos.js
-grep "^const .*Infos" source.html > Sources/SampleSkillInfos.js
+grep -E "(var|const)\s+heroInfos\s*=" source.html > Sources/SampleHeroInfos.js
+grep -E "(var|const)\s+(weapon|support|special|passiveA|passiveB|passiveC|passiveS)Infos\s*=" source.html > Sources/SampleSkillInfos.js
 
 rm source.html
