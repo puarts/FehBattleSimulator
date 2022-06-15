92ce8f5364c3908083f4bbb617a200c012e2294d
puarts <71858166+puarts@users.noreply.github.com>
2022-06-15 21:28:34 +0900

    update_sample_files.shで隊長スキルを加味するよう修正

--- update_sample_files.sh
+++ update_sample_files.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-curl -k "https://puarts.com/?pid=1469" > source.html
+curl -k "https://puarts.com/?pid=1762" > source.html
 
 grep -E "(var|const)\s+heroInfos\s*=" source.html > Sources/SampleHeroInfos.js
-grep -E "(var|const)\s+(weapon|support|special|passiveA|passiveB|passiveC|passiveS)Infos\s*=" source.html > Sources/SampleSkillInfos.js
+grep -E "(var|const)\s+(weapon|support|special|passiveA|passiveB|passiveC|passiveS|captain)Infos\s*=" source.html > Sources/SampleSkillInfos.js
 
 rm source.html
