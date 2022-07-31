619b11d0f7889590e97b420c165642f6c5fc8427
puarts <71858166+puarts@users.noreply.github.com>
2022-07-31 16:43:52 +0900

    htmlファイルのデプロイしやすいよう修正

--- Deploy.bat
+++ Deploy.bat
@@ -2,6 +2,7 @@
 
 set battle_simulator_filenames=GlobalDefinitions,Utilities,Logger,Skill,BattleMapElement,Tile,BattleMap,BattleMapSettings,Structures,Cell,Table,HeroInfo,Unit,UnitManager,GlobalBattleContext,DamageCalculationUtility,DamageCalculator,PostCombatSkillHander,DamageCalculatorWrapper,BeginningOfTurnSkillHandler,SkillDatabase,HeroDatabase,TurnSetting,AudioManager,AetherRaidDefensePresets,SettingManager,AppData,Main_ImageProcessing,Main_OriginalAi,Main_MouseAndTouch,BattleSimulatorBase,VueComponents
 
+
 rem 飛空城シミュレーター
 call %~dp0MergeSourcesAndCompress.bat FehBattleSimulator %battle_simulator_filenames%,AetherRaidSimulatorMain
 
@@ -21,9 +22,20 @@ rem ダメージ計算機
 call %~dp0MergeSourcesAndCompress.bat FehDamageCalculator GlobalDefinitions_Debug,Utilities,Logger,Skill,BattleMapElement,Tile,BattleMap,GlobalBattleContext,Structures,Table,HeroInfo,Unit,UnitManager,SkillDatabase,HeroDatabase,DamageCalculationUtility,DamageCalculator,PostCombatSkillHander,DamageCalculatorWrapper,BeginningOfTurnSkillHandler,SampleSkillInfos,SampleHeroInfos,VueComponents,KeyRepeatHandler,DamageCalculatorMain
 
 rem その他ツールで使うファイル
+echo other files
 set copyfiles=%battle_simulator_filenames% StatusCalcMain SampleSkillInfos SampleHeroInfos KeyRepeatHandler DamageCalculatorMain HeroStatusClustererMain
 for %%n in (%copyfiles%) do (
     copy %~dp0Sources\%%n.js %destination%\%%n.js
+)
+
+rem HTMLファイル
+echo html files
+set trunk_root=%~dp0..\..\trunk
+set root=%trunk_root%\Websites\puarts.com
+set html_destination=%root%\blog\entries
+set copyfiles=AetherRaidSimulator ArenaSimulator DamageCalculator SummonerDuelsSimulator UnitBuilder
+for %%n in (%copyfiles%) do (
+    copy %~dp0Sources\%%n.html %html_destination%\%%n.html
 )
 
 pause
