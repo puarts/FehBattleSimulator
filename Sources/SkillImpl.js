3661b673fe3e72793f529ed292a2458ec14b620a
yakitori-776 <yakitori776@gmail.com>
2024-08-24 07:06:42 +0900

    盛夏の神宝の錬成を実装

--- Sources/SkillImpl.js
+++ Sources/SkillImpl.js
@@ -955,7 +955,6 @@
 // アッサルの槍
 {
     let skillId = getSpecialRefinementSkillId(Weapon.SpearOfAssal);
-    // pause: 特殊錬成を実装
     // Spear of Assal can be upgraded with the additional effect Spear of Assal W
     AT_START_OF_COMBAT_HOOKS.addSkill(skillId, () => new SkillEffectNode(
         // "If unit is within 3 spaces of an ally,
@@ -1042,17 +1041,90 @@
 
 // 盛夏の神宝
 {
-    let skillId = Weapon.SunsPercussors;
-    applySkillEffectForUnitFuncMap.set(skillId,
-        function (targetUnit, enemyUnit, calcPotentialDamage) {
-            if (targetUnit.getEvalSpdInPrecombat() > enemyUnit.getEvalSpdInPrecombat() ||
-                enemyUnit.battleContext.restHpPercentage === 100) {
-                targetUnit.addAtkSpdSpurs(5);
-                targetUnit.battleContext.invalidatesAbsoluteFollowupAttack = true;
-                targetUnit.battleContext.invalidatesInvalidationOfFollowupAttack = true;
-            }
-        }
-    );
+    let skillId = getSpecialRefinementSkillId(Weapon.SunsPercussors);
+    // Sun's Percussors can be upgraded with the additional effect Suns Percussors W
+    AT_START_OF_COMBAT_HOOKS.addSkill(skillId, () => new SkillEffectNode(
+        // "If unit initiates combat or is within 3 spaces of an ally,
+        IF_NODE(OR_NODE(DOES_UNIT_INITIATE_COMBAT_NODE, IS_TARGET_WITHIN_3_SPACES_OF_TARGETS_ALLY_NODE),
+            // grants bonus to unit's Atk/Spd/Def/Res = number of allies within 3 spaces of unit × 2, + 4 (max 10) and
+            new NumThatIsNode(
+                new GrantsAllStatsPlusNToUnitDuringCombatNode(READ_NUM_NODE),
+                new EnsureMaxNode(ADD_NODE(MULT_NODE(new NumOfTargetsAlliesWithinNSpacesNode(3), 2), 4), 10),
+            ),
+            new AppliesSkillEffectsAfterStatusFixedNode(
+                // deals damage = 15% of unit's Spd during combat (excluding area-of-effect Specials).
+                new UnitDealsDamageExcludingAoeSpecialsNode(UNITS_SPD_DURING_COMBAT_NODE),
+            ),
+            // If unit initiates combat,
+            IF_NODE(DOES_UNIT_INITIATE_COMBAT_NODE,
+                // reduces damage from foe's first attack by 50% during combat
+                new ReducesDamageFromFoesFirstAttackByNPercentDuringCombatNode(50),
+                // ("first attack" normally means only the first strike; for effects that grant "unit attacks twice," it means the first and second strikes),
+                // and also,
+                // if foe's attack can trigger foe's Special,
+                IF_NODE(CAN_UNITS_ATTACK_TRIGGER_SPECIAL_NODE,
+                    // inflicts Special cooldown count+1 on foe before foe's first attack (cannot exceed the foe's maximum Special cooldown).".
+                    new InflictsSpecialCooldownCountPlusNOnTargetsFoeBeforeTargetsFoesFirstAttack(1),
+                ),
+            ),
+        ),
+    ));
+}
+
+{
+    let skillId = getRefinementSkillId(Weapon.SunsPercussors);
+    // When upgraded, the description of Sun's Percussors becomes "Accelerates Special trigger (cooldown count-1).
+    AT_START_OF_COMBAT_HOOKS.addSkill(skillId, () => new SkillEffectNode(
+        // At start of combat,
+        // if unit's Spd ≥ foe's Spd-5 or if foe's HP ≥ 75%,
+        IF_NODE(OR_NODE(
+                GT_NODE(UNITS_EVAL_SPD_AT_START_OF_COMBAT_NODE, SUB_NODE(FOES_EVAL_SPD_AT_START_OF_COMBAT_NODE, 5)),
+                IS_FOES_HP_GTE_75_PERCENT_AT_START_OF_COMBAT_NODE
+            ),
+            // grants Atk/Spd+5 and Def/Res+4 to unit,
+            new GrantsStatsPlusToUnitDuringCombatNode(5, 5, 4, 4),
+            // neutralizes effects that guarantee foe's follow-up attacks and effects that prevent unit's follow-up attacks,
+            NULL_UNIT_FOLLOW_UP_NODE,
+            // neutralizes effects that inflict "Special cooldown charge -X" on unit,
+            NEUTRALIZES_EFFECTS_THAT_INFLICT_SPECIAL_COOLDOWN_CHARGE_MINUS_X_ON_UNIT,
+            // and reduces the percentage of foe's non-Special "reduce damage by X%" skills by 50% during combat (excluding area-of-effect Specials).
+            REDUCES_PERCENTAGE_OF_FOES_NON_SPECIAL_DAMAGE_REDUCTION_BY_50_PERCENT_DURING_COMBAT_NODE,
+        )
+    ));
+
+    FOR_ALLIES_GRANTS_STATS_PLUS_TO_ALLIES_DURING_COMBAT_HOOKS.addSkill(skillId, () => new SkillEffectNode(
+        // Grants Atk/Spd+4 and
+        // to allies within 2 spaces of unit during their combat.".
+        IF_NODE(IS_TARGET_WITHIN_2_SPACES_OF_SKILL_OWNER_NODE,
+            new GrantsStatsPlusToTargetDuringCombatNode(4, 4, 0, 0),
+        ),
+    ));
+
+    FOR_ALLIES_GRANTS_EFFECTS_TO_ALLIES_DURING_COMBAT_HOOKS.addSkill(skillId, () => new SkillEffectNode(
+        // "reduces the percentage of foe's non-Special 'reduce damage by X%' skills by 50% (excluding area-of-effect Specials)"
+        // to allies within 2 spaces of unit during their combat.".
+        IF_NODE(IS_TARGET_WITHIN_2_SPACES_OF_SKILL_OWNER_NODE,
+            REDUCES_PERCENTAGE_OF_FOES_NON_SPECIAL_DAMAGE_REDUCTION_BY_50_PERCENT_DURING_COMBAT_NODE,
+        ),
+    ));
+}
+
+{
+    let skillId = getNormalSkillId(Weapon.SunsPercussors);
+    // Accelerates Special trigger (cooldown count-1).
+    AT_START_OF_COMBAT_HOOKS.addSkill(skillId, () => new SkillEffectNode(
+        // At start of combat,
+        // if unit's Spd > foe's Spd or if foe's HP = 100%,
+        IF_NODE(OR_NODE(
+                GT_NODE(UNITS_EVAL_SPD_AT_START_OF_COMBAT_NODE, FOES_EVAL_SPD_AT_START_OF_COMBAT_NODE),
+                EQ_NODE(FOES_HP_PERCENTAGE_AT_START_OF_COMBAT_NODE, 100),
+            ),
+            // grants Atk/Spd+5 to unit during combat and
+            GRANTS_ATK_SPD_PLUS_5_TO_UNIT_DURING_COMBAT_NODE,
+            // neutralizes effects that guarantee foe's follow-up attacks and effects that prevent unit's follow-up attacks.
+            NULL_UNIT_FOLLOW_UP_NODE,
+        ),
+    ));
 }
 
 // フェイルノート
