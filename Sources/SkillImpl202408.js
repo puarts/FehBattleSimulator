1eb5f51e3ec694ac67b60cbe84ec22566ccd38bb
yakitori-776 <yakitori776@gmail.com>
2024-09-16 09:33:30 +0900

    氷フィヨルムの武器のバグ修正

--- Sources/SkillImpl202408.js
+++ Sources/SkillImpl202408.js
@@ -290,8 +290,10 @@
         IF_NODE(OR_NODE(DOES_FOE_INITIATE_COMBAT_NODE, IS_FOES_HP_GTE_75_PERCENT_AT_START_OF_COMBAT_NODE),
             // grants bonus to unit's Atk/Spd/Def/Res = 5 + number of allies within 3 spaces of unit × 3 (max 14),
             new NumThatIsNode(
-                new GrantsAllStatsPlusNToTargetDuringCombatNode(ADD_NODE(5, READ_NUM_NODE)),
-                new EnsureMaxNode(MULT_NODE(new NumOfTargetsAlliesWithinNSpacesNode(3), 3), 14),
+                new GrantsAllStatsPlusNToTargetDuringCombatNode(READ_NUM_NODE),
+                new EnsureMaxNode(
+                    ADD_NODE(5, MULT_NODE(new NumOfTargetsAlliesWithinNSpacesNode(3), 3)), 14
+                ),
             ),
             // reduces damage from foe's attacks by 20% of unit's Spd (excluding area-of-effect Specials),
             new AppliesSkillEffectsAfterStatusFixedNode(
