e96d989dd1dbe7a0962de29162b4f38a06b9d1e7
yakitori-776 <yakitori776@gmail.com>
2024-12-12 01:33:05 +0900

    響・遠距離反撃を実装

--- Sources/SkillConstants.js
+++ Sources/SkillConstants.js
@@ -3563,6 +3563,7 @@ const PassiveX = {
     FarTraceEcho: 2908, // 響・遠影
     NullCDisruptE: 2953, // 響・見切り反撃不可
     TimePulseEcho: 3013, // 響・始まりの鼓動
+    DCounterEcho: 3094, // 響・遠距離反撃
 };
 
 const Captain = {
