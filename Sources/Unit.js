00973497d29dabd430760c4e2dbf4dc511b44031
yakitori-776 <yakitori776@gmail.com>
2025-01-14 22:37:08 +0900

    バグの修正

--- Sources/Unit.js
+++ Sources/Unit.js
@@ -5515,7 +5515,7 @@ class Unit extends BattleMapElement {
         this.updateStatusBySkillsExceptWeapon();
 
         for (let skillId of this.enumerateSkills()) {
-            this.maxHpWithSkills += HP_WITH_SKILLS_MAP.get(skillId) ?? 0;
+            this.maxHpWithSkillsWithoutAdd += HP_WITH_SKILLS_MAP.get(skillId) ?? 0;
             this.atkWithSkills += ATK_WITH_SKILLS_MAP.get(skillId) ?? 0;
             this.spdWithSkills += SPD_WITH_SKILLS_MAP.get(skillId) ?? 0;
             this.defWithSkills += DEF_WITH_SKILLS_MAP.get(skillId) ?? 0;
