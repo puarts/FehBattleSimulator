8b39549291c8e8ca5155a0693f0e2f0af5a6a000
yakitori-776 <yakitori776@gmail.com>
2026-02-02 02:23:55 +0900

    テスト関連の更新

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20.11.1
+FROM node:22
 
 COPY ./package.json /
 RUN npm install
--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,15 @@
 ./create_tests.sh
 
 TARGET_FILE=All.test.js
-npm test
+
+# 引数の数($#)が0より大きいかチェック
+if [ $# -gt 0 ]; then
+  # 引数がある場合: test:only に引数をそのまま渡す
+  # "$@" を使うことで、スペースを含む引数(例: -t "closest")も正しく渡されます
+  npm run test:only -- "$@"
+else
+  # 引数がない場合: 従来の npm test (jest + eslint) を実行
+  npm test
+fi
+
 rm $TARGET_FILE
