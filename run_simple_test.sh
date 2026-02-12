ab370f7b23bad16f8847a082da6403ed6cd38a41
yakitori-776 <yakitori776@gmail.com>
2026-02-13 08:12:16 +0900

    テスト実行用スクリプト `run_simple_test.sh` を追加

---
+++ run_simple_test.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+# add files
+SOURCE_FILE_NAMES=(
+    GlobalDefinitions
+    Utilities
+    Logger
+    SkillConstants
+    Skill
+    SkillEffectCore
+    SkillEffectEnv
+    SkillEffect
+    SkillEffectField
+    SkillEffectUnit
+    SkillEffectBattleContext
+    SkillEffectHooks
+    SkillEffectRegistrar
+    BattleMapElement
+    Tile
+    Structures
+    Cell
+    Table
+    HeroInfoConstants
+    HeroInfo
+    UnitConstants
+    BattleContext
+    Unit
+    UnitManager
+    BattleMap
+    GlobalBattleContext
+    DamageCalculationUtility
+    DamageCalculator
+    PostCombatSkillHander
+    DamageCalculatorWrapper
+    BeginningOfTurnSkillHandler
+    SkillDatabase
+    HeroDatabase
+    SampleSkillInfos
+    SampleHeroInfos
+    SkillEffectAliases
+    SkillImpl
+    SkillImpl202408
+    SkillImpl202501
+    SkillImpl202601
+    TestUtilities
+    )
+TEST_UTIL_FILE_NAMES=(
+    TestGlobals
+    )
+TEST_FILE_NAMES=(
+    SimpleUtility
+    )
+
+TARGET_FILE=SimpleUtility.test.js
+touch ./$TARGET_FILE
+cp /dev/null ./$TARGET_FILE
+
+for name in ${SOURCE_FILE_NAMES[@]}; do
+    cat ./Sources/${name}.js >> ./$TARGET_FILE
+done
+
+for name in ${TEST_UTIL_FILE_NAMES[@]}; do
+    cat ./Tests/${name}.js >> ./$TARGET_FILE
+done
+
+for name in ${TEST_FILE_NAMES[@]}; do
+    cat ./Tests/${name}.test.js >> ./$TARGET_FILE
+done
+
+# Run the test
+npx jest $TARGET_FILE --silent=false --verbose true
+
+# Clean up
+rm $TARGET_FILE
