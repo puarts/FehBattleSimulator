c5bf3dd0e42fc80ca47a7965560d9b00d00554df
yakitori-776 <yakitori776@gmail.com>
2026-03-28 01:04:12 +0900

    refactor: BattleMapSkillSupport.jsのre-exportモジュール追加
    BattleMap.jsの循環依存回避用の中間モジュール

---
+++ Sources/BattleMapSkillSupport.js
@@ -0,0 +1,4 @@
+import { BattleMapEnv, getSkillLogLevel } from './SkillEffectEnv.js';
+import { AOE_SPECIAL_SPACES_HOOKS } from './SkillEffectHooks.js';
+
+export { BattleMapEnv, getSkillLogLevel, AOE_SPECIAL_SPACES_HOOKS };
